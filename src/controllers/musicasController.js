571bb265128007a1731018f1b1ea419ab650f3d5
Elisson Silva <elissonsilva.dev@gmail.com>
2026-04-11 21:29:12 -0300

    add try/cath

--- src/controllers/musicasController.js
+++ src/controllers/musicasController.js
@@ -2,7 +2,12 @@ const musicas = require("../models/musicasModel.js");
 
 module.exports = class getAllMusicasController {
   static async getAllMusicas(req, res) {
-    const musicasList = await musicas.getAllMusicas();
-    res.json(musicasList);
+    try {
+      const musicasList = await musicas.getAllMusicas();
+      res.json(musicasList);
+    } catch (err) {
+      console.error("❌ Erro:", err.message);
+      res.status(500).json({ error: err.message });
+    }
   }
 };
